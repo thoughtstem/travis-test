bb3689a8b16800b5be68a5a511f8131bd1f20684
Stephen <stephen@thoughtstem.com>
2019-02-27 11:05:17 -0800

    first

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "travis-test")
+(define deps '("base" "game-engine"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/travis-test.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(thoughtstem))
---
+++ scribblings/travis-test.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[travis-test
+                    racket/base]]
+
+@title{travis-test}
+@author{thoughtstem}
+
+@defmodule[travis-test]
+
+Package Description Here
